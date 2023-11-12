0c4490b5434143a8ddf06211d73f39118f801897
kilmarcruz <00043523@uca.edu.sv>
2023-11-12 16:22:50 -0600

    :memo: se mejoro las funcion de reseñas y ahora se guardaran en un archivo txt

--- Reserva.cpp
+++ Reserva.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
+#include <fstream> //libreria para los archivos txt
 using namespace std;
 void comentariosUsuario();
 void resenia();
 
 int main()
 {
-    int opcion,repiteMenu,calificarServicio;
+    int opcion,repiteMenu;
     
 do {
     cout << "Seleccione un dia para ver la disponibilidad de las mesas\n";
     cout << "1.Dejar un comentario sobre nustros servicios: \n";
     cout << "2.Ver resenias:\n";
     cout << "3.Caficar servicion: \n";
-    cin >> opcion;
+    cin  >> opcion;
+    system("cls");
     switch (opcion)
     {
     
@@ -41,21 +43,35 @@ cout<<"Muchas gracias por preferirnos :)";
 void comentariosUsuario(){
 string comentario,nombreCliente;
 int opcion;
+
 cout<<"Ingrese su nombre por favor: ";
 cin>>nombreCliente;
-cout<<"Esrcribra su comentario por favor y presione ENTER cuando termine: ";
-cin>>comentario;
+cin.ignore();//ignorara el bufer antes de usar el getline
+cout<<"Esrcribra su comentario por favor y presione ENTER cuando termine: \n";
+getline(cin, comentario);//guardara toda la reseña del usuario.
 cout<<"Esta seguro de dejar este comentario? (1=si/2=no)\n";
 cin>>opcion;
-if (opcion == 1)
-{
-    cout<<"Muchas gracias por brindarnos sus comentarios, los tomaremos en cuenta para mejorar la experencia.\n";
+
+if (opcion == 1) {
+        cout << "Muchas gracias por brindarnos sus comentarios, los tomaremos en cuenta para mejorar la experiencia.\n";
+
+        // Guardar el comentario en un archivo
+        ofstream archivo_comentarios("comentarios.txt", ios::app); // Abre el archivo en modo append
+        if (archivo_comentarios.is_open()) {
+            archivo_comentarios << "Nombre: " << nombreCliente << "\nComentario: " << comentario << "\n\n";
+            archivo_comentarios.close();
+        } else {
+            cout << "No se pudo abrir el archivo para guardar el comentario.\n";
+        }
+    }
 }
-}
+
+
 
 void resenia(){
 int  calificacion, estrellas;
 string nombre;
+
 cout << "ingrese su nombre\n";
 cin >> nombre;
 cout << "califique nuestros servicios del 1 al 5, siendo 5 la maxima nota: \n";
@@ -67,6 +83,7 @@ if (estrellas >= 1 && estrellas <= 5)
     {
         cout << "*";
     }
+    calificacion = estrellas;
 }
 else
     cout << "ponga un numero dentro del rango por favor\n";
