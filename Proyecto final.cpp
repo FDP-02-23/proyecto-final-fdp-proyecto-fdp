7ad02a6214c1069268c8b51e24e8a3e3fdd0a5c8
Yuliana <128511401+YulianaDeras@users.noreply.github.com>
2023-11-22 15:05:31 -0600

    Create Proyecto final.cpp

---
+++ Proyecto final.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+struct Reserva {
+    int id;
+    string fecha;
+    int numero_personas;
+
+   // Constructor para facilitar la creación de reservas
+    Reserva(int _id, const string& _fecha, int _numero_personas)
+        : id(_id), fecha(_fecha), numero_personas(_numero_personas) {}
+};
